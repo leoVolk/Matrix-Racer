0df587b2d7d30f900d9f095bf80dc2aabebe05ee
Leo Volkmann <leovolkmann@googlemail.com>
2019-12-19 17:39:03 +0100

    inital commit

---
+++ Matrix Racer/Assets/Scripts/Avatar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Avatar : MonoBehaviour {
+
+	public ParticleSystem shape, trail, burst, explosion;
+
+	private Player player;
+
+	public float deathCountdown = -1f;
+	public GameObject spaceship;
+
+	private void Awake () {
+		player = transform.root.GetComponent<Player>();
+		
+	}
+
+	private void OnTriggerEnter (Collider collider) {
+		if (deathCountdown < 0f) {
+			spaceship.SetActive(false);
+			shape.enableEmission = false;
+			trail.enableEmission = false;
+			burst.Emit(burst.maxParticles);
+			explosion.Emit(explosion.maxParticles);
+			deathCountdown = burst.startLifetime;
+		}
+	}
+	
+	private void Update () {
+		if (deathCountdown >= 0f) {
+			deathCountdown -= Time.deltaTime;
+			if (deathCountdown <= 0f) {
+				spaceship.SetActive(true);
+				deathCountdown = -1f;
+				shape.enableEmission = true;
+				trail.enableEmission = true;
+				player.Die();
+			}
+		}
+	}
+}
